953b65ec7a30b8e6d4212e0a814a0f37d133a6aa
Iury Ferreira Santana <criticow@outlook.com>
2025-03-08 12:33:58 -0300

    updated transform properties

--- src/ngyn/components/transform.cpp
+++ src/ngyn/components/transform.cpp
@@ -2,23 +2,123 @@
 
 using namespace ngyn;
 
-Transform::Transform(TransformCreateInfo createInfo)
+Transform::Transform(CreateInfo createInfo) :
+  _position(createInfo.position),
+  _size(createInfo.size),
+  _scale(createInfo.scale),
+  _rotation(createInfo.rotation),
+  _layerMode(createInfo.layerMode),
+  _layer(createInfo.layer),
+  _model(glm::mat4(1.0f)),
+  _zIndex(0.0f)
 {
-  this->position = createInfo.position;
-  this->size = createInfo.size;
-  this->rotation = createInfo.rotation;
-  this->layer = createInfo.layer;
-
-  this->model = glm::mat4(1.0f);
-  this->zIndex = 0.0f;
-
   this->updateModel();
+}
+
+const glm::vec2 &ngyn::Transform::position()
+{
+  return _position;
+}
+
+const glm::vec2 &ngyn::Transform::size()
+{
+  return _size;
+}
+
+const glm::vec2 &ngyn::Transform::scale()
+{
+  return _scale;
+}
+
+const float &ngyn::Transform::rotation()
+{
+  return _rotation;
+}
+
+const Transform::LayerMode &ngyn::Transform::layerMode()
+{
+  return _layerMode;
+}
+
+const int &ngyn::Transform::layer()
+{
+  return _layer;
+}
+
+const glm::mat4 &ngyn::Transform::model()
+{
+  return _model;
+}
+
+const float &ngyn::Transform::zIndex()
+{
+  return _zIndex;
+}
+
+void ngyn::Transform::setPosition(const glm::vec2 &position)
+{
+  if(_position == position) return;
+
+  _position = position;
+  updateModel();
+}
+
+void ngyn::Transform::setSize(const glm::vec2 &size)
+{
+  if(_size == size) return;
+
+  _size = size;
+  updateModel();
+}
+
+void ngyn::Transform::setScale(const glm::vec2 &scale)
+{
+  if(_scale == scale) return;
+
+  _scale = scale;
+  updateModel();
+}
+
+void ngyn::Transform::setRotation(const float &rotation)
+{
+  if(_rotation == rotation) return;
+
+  _rotation = rotation;
+  updateModel();
+}
+
+void ngyn::Transform::setLayerMode(const LayerMode &layerMode)
+{
+  if(_layerMode == layerMode) return;
+
+  _layerMode = layerMode;
+  updateModel();
+}
+
+void ngyn::Transform::setLayer(const int &layer)
+{
+  if(_layer == layer) return;
+
+  _layer = layer;
+  updateModel();
 }
 
 void Transform::updateModel()
 {
-  this->model = glm::mat4(1.0f);
+  _model = glm::mat4(1.0f);
+  _zIndex = _layer;
 
-  this->model = glm::translate(this->model, this->position);
-  this->model = glm::scale(this->model, this->size);
+  if(_layerMode == LayerMode::Dynamic)
+  {
+    float yOffset = (_position.y + _size.y) * 0.00001f;
+    _zIndex += yOffset;
+  }
+
+  _model = glm::translate(_model, glm::vec3(_position, 0.0f));
+
+  _model = glm::translate(_model, glm::vec3(_size * 0.5f, 0.0f));
+  _model = glm::rotate(_model, glm::radians(_rotation), glm::vec3(0.0f, 0.0f, 1.0f));
+  _model = glm::translate(_model, glm::vec3(_size * -0.5f, 0.0f));
+
+  _model = glm::scale(_model, glm::vec3(_size * _scale, 0.0f));
 }
--- src/ngyn/components/transform.hpp
+++ src/ngyn/components/transform.hpp
@@ -2,32 +2,66 @@
 
 #include "../../ngynpch.hpp"
 
+/*
+  TODO: Add origin for scale, rotation?
+ */
+
 namespace ngyn
 {
-  struct TransformCreateInfo
-  {
-    glm::vec3 position;
-    glm::vec3 size;
-    glm::vec3 scale;
-    glm::vec3 rotation;
-    int layer;
-  };
-
   class Transform
   {
     public:
-    Transform(void) = default;
-    Transform(TransformCreateInfo createInfo);
+    enum LayerMode
+    {
+      Simple,
+      Dynamic
+    };
 
-    glm::vec3 position;
-    glm::vec3 size;
-    glm::vec3 scale;
-    glm::vec3 rotation;
-    int layer;
-    glm::mat4 model;
-    float zIndex;
+    struct CreateInfo
+    {
+      glm::vec2 position = glm::vec2(0.0f);
+      glm::vec2 size = glm::vec2(0.0f);
+      glm::vec2 scale = glm::vec2(1.0f);
+      float rotation = 0.0f;
+      LayerMode layerMode = LayerMode::Simple;
+      int layer = 0;
+    };
+
+    public:
+    Transform(void) = default;
+    Transform(CreateInfo createInfo);
+    
+    // Getters
+    const glm::vec2 &position();
+    const glm::vec2 &size();
+    const glm::vec2 &scale();
+    const float &rotation();
+    const LayerMode &layerMode();
+    const int &layer();
+    const glm::mat4 &model();
+    const float &zIndex();
+
+    // Setters
+    void setPosition(const glm::vec2 &position);
+    void setSize(const glm::vec2 &size);
+    void setScale(const glm::vec2 &scale);
+    void setRotation(const float &rotation);
+    void setLayerMode(const LayerMode &layerMode);
+    void setLayer(const int &layer);
 
     private:
+    // CreateInfo properties
+    glm::vec2 _position;
+    glm::vec2 _size;
+    glm::vec2 _scale;
+    float _rotation;
+    LayerMode _layerMode;
+    int _layer;
+
+    // Other properties
+    glm::mat4 _model;
+    float _zIndex;
+
     void updateModel();
   };
 };
