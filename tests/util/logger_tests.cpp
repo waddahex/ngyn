1c9c197d82230dfc49f4f1458ede74b1f8612591
Iury Ferreira Santana <criticow@outlook.com>
2025-03-12 09:28:06 -0300

    logger tests updated

--- tests/util/logger_tests.cpp
+++ tests/util/logger_tests.cpp
@@ -1,12 +1,13 @@
-#include <catch2/catch_test_macros.hpp>
-#include <ngyn/util/logger.hpp>
-#include <ngyn/util/files.hpp>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+#include <ngyn/ngyn.hpp>
 
 using namespace ngyn;
 
-TEST_CASE("stdout content", "[logger]")
+TEST_CASE("stdout content")
 {
-  SECTION("stdout should contain the debug logger output")
+  SUBCASE("stdout should contain the debug logger output")
   {
     std::stringstream buffer;
     std::streambuf *old = std::cout.rdbuf(buffer.rdbuf());
@@ -16,10 +17,10 @@ TEST_CASE("stdout content", "[logger]")
     std::cout.rdbuf(old);
     std::string capturedOutput = buffer.str();
 
-    REQUIRE(value + "\n" == capturedOutput);
+    CHECK(value + "\n" == capturedOutput);
   }
 
-  SECTION("stdout should contain the warning logger output")
+  SUBCASE("stdout should contain the warning logger output")
   {
     std::stringstream buffer;
     std::streambuf *old = std::cout.rdbuf(buffer.rdbuf());
@@ -29,10 +30,10 @@ TEST_CASE("stdout content", "[logger]")
     std::cout.rdbuf(old);
     std::string capturedOutput = buffer.str();
 
-    REQUIRE(value + "\n" == capturedOutput);
+    CHECK(value + "\n" == capturedOutput);
   }
 
-  SECTION("stdout should contain the error logger output")
+  SUBCASE("stdout should contain the error logger output")
   {
     std::stringstream buffer;
     std::streambuf *old = std::cout.rdbuf(buffer.rdbuf());
@@ -42,11 +43,11 @@ TEST_CASE("stdout content", "[logger]")
     std::cout.rdbuf(old);
     std::string capturedOutput = buffer.str();
 
-    REQUIRE(value + "\n" == capturedOutput);
+    CHECK(value + "\n" == capturedOutput);
   }
 }
 
-TEST_CASE("Save log to file", "[logger]")
+TEST_CASE("Save log to file")
 {
   ngyn::logger.setDirectory("logs");
   std::filesystem::remove_all("logs");
@@ -61,25 +62,25 @@ TEST_CASE("Save log to file", "[logger]")
 
   std::filesystem::path filename = std::format("{}-{}-{}", year, month, day);
 
-  SECTION("Should not save to file if mode equal LoggerMode::Console")
+  SUBCASE("Should not save to file if mode equal LoggerMode::Console")
   {
     ngyn::logger.setMode(LoggerMode::Console);
     ngyn::logger.error("Save test");
 
-    REQUIRE(!std::filesystem::exists("logs" / filename));
+    CHECK(!std::filesystem::exists("logs" / filename));
   }
 
-  SECTION("Should save to file if mode equal LoggerMode::Quiet")
+  SUBCASE("Should save to file if mode equal LoggerMode::Quiet")
   {
     ngyn::logger.setMode(LoggerMode::Quiet);
     ngyn::logger.error("LoggerMode::Quiet test");
 
     std::string logData = files::read(std::filesystem::path("logs" / filename));
 
-    REQUIRE(logData.ends_with("LoggerMode::Quiet test\n"));
+    CHECK(logData.ends_with("LoggerMode::Quiet test\n"));
   }
 
-  SECTION("Should save to file and print to console if mode equal LoggerMode::All")
+  SUBCASE("Should save to file and print to console if mode equal LoggerMode::All")
   {
     ngyn::logger.setMode(LoggerMode::All);
 
@@ -93,11 +94,11 @@ TEST_CASE("Save log to file", "[logger]")
 
     std::string logData = files::read(std::filesystem::path("logs" / filename));
 
-    REQUIRE(logData.ends_with("LoggerMode::All test\n"));
-    REQUIRE(value + "\n" == capturedOutput);
+    CHECK(logData.ends_with("LoggerMode::All test\n"));
+    CHECK(value + "\n" == capturedOutput);
   }
 
-  SECTION("Should not save or log debug logs if level is lower than LoggerLevel::Debug")
+  SUBCASE("Should not save or log debug logs if level is lower than LoggerLevel::Debug")
   {
     ngyn::logger.setLevel(LoggerLevel::Warning);
 
@@ -111,11 +112,11 @@ TEST_CASE("Save log to file", "[logger]")
 
     std::string logData = files::read(std::filesystem::path("logs" / filename));
 
-    REQUIRE(!logData.ends_with("LoggerLevel::Warning test\n"));
-    REQUIRE(value + "\n" != capturedOutput);
+    CHECK(!logData.ends_with("LoggerLevel::Warning test\n"));
+    CHECK(value + "\n" != capturedOutput);
   }
 
-  SECTION("Should not save or log warning logs if level is lower than LoggerLevel::Warning")
+  SUBCASE("Should not save or log warning logs if level is lower than LoggerLevel::Warning")
   {
     ngyn::logger.setLevel(LoggerLevel::Error);
 
@@ -129,11 +130,11 @@ TEST_CASE("Save log to file", "[logger]")
 
     std::string logData = files::read(std::filesystem::path("logs" / filename));
 
-    REQUIRE(!logData.ends_with("LoggerLevel::Error test\n"));
-    REQUIRE(value + "\n" != capturedOutput);
+    CHECK(!logData.ends_with("LoggerLevel::Error test\n"));
+    CHECK(value + "\n" != capturedOutput);
   }
 
-  SECTION("Should not save or log anything logs if level is lower than LoggerLevel::Error")
+  SUBCASE("Should not save or log anything logs if level is lower than LoggerLevel::Error")
   {
     ngyn::logger.setLevel(LoggerLevel::Disabled);
 
@@ -147,92 +148,92 @@ TEST_CASE("Save log to file", "[logger]")
 
     std::string logData = files::read(std::filesystem::path("logs" / filename));
 
-    REQUIRE(!logData.ends_with("LoggerLevel::Disabled test\n"));
-    REQUIRE(value + "\n" != capturedOutput);
+    CHECK(!logData.ends_with("LoggerLevel::Disabled test\n"));
+    CHECK(value + "\n" != capturedOutput);
   }
 }
 
-TEST_CASE("Replacing values correctly", "[logger]")
+TEST_CASE("Replacing values correctly")
 {
   std::string tail = "\033[0m";
   ngyn::logger.setMode(LoggerMode::Quiet);
   ngyn::logger.setLevel(LoggerLevel::Debug);
 
-  SECTION("Replaces 0 from boolean with false")
+  SUBCASE("Replaces 0 from boolean with false")
   {
     auto value = ngyn::logger.debug(false);
-    REQUIRE(value.ends_with("false" + tail));
+    CHECK(value.ends_with("false" + tail));
   }
 
-  SECTION("Replaces 1 from boolean with true")
+  SUBCASE("Replaces 1 from boolean with true")
   {
     auto value = ngyn::logger.debug(true);
-    REQUIRE(value.ends_with("true" + tail));
+    CHECK(value.ends_with("true" + tail));
   }
 
-  SECTION("Prints a single value correctly")
+  SUBCASE("Prints a single value correctly")
   {
     auto value = ngyn::logger.debug(1);
-    REQUIRE(value.ends_with("1" + tail));
+    CHECK(value.ends_with("1" + tail));
   }
 
-  SECTION("Replaces values using args order")
+  SUBCASE("Replaces values using args order")
   {
     auto value = ngyn::logger.debug("{} {} {}", 0, 1, 2);
-    REQUIRE(value.ends_with("0 1 2" + tail));
+    CHECK(value.ends_with("0 1 2" + tail));
   }
 
-  SECTION("Replaces values using provided indexed placeholders in order")
+  SUBCASE("Replaces values using provided indexed placeholders in order")
   {
     auto value = ngyn::logger.debug("{0} {1} {2}", 2, 3, 4);
-    REQUIRE(value.ends_with("2 3 4" + tail));
+    CHECK(value.ends_with("2 3 4" + tail));
   }
 
-  SECTION("Replaces values using provided indexed placeholders out of order")
+  SUBCASE("Replaces values using provided indexed placeholders out of order")
   {
     auto value = ngyn::logger.debug("{2} {0} {1}", 0, 1, 2);
-    REQUIRE(value.ends_with("2 0 1" + tail));
+    CHECK(value.ends_with("2 0 1" + tail));
   }
 
-  SECTION("Replaces values using args in order and provided indexed placeholders in order")
+  SUBCASE("Replaces values using args in order and provided indexed placeholders in order")
   {
     auto value = ngyn::logger.debug("{} {0} {} {1} {} {2}", 0, 1, 2);
-    REQUIRE(value.ends_with("0 0 1 1 2 2" + tail));
+    CHECK(value.ends_with("0 0 1 1 2 2" + tail));
   }
 
-  SECTION("Replaces values using args in order and provided indexed placeholders out of order")
+  SUBCASE("Replaces values using args in order and provided indexed placeholders out of order")
   {
     auto value = ngyn::logger.debug("{} {2} {} {0} {} {1}", 0, 1, 2);
-    REQUIRE(value.ends_with("0 2 1 0 2 1" + tail));
+    CHECK(value.ends_with("0 2 1 0 2 1" + tail));
   }
 }
 
-TEST_CASE("Output color should match", "[logger]")
+TEST_CASE("Output color should match")
 {
   ngyn::logger.setMode(LoggerMode::Quiet);
   ngyn::logger.setLevel(LoggerLevel::Debug);
 
-  SECTION("Debug output should have teal color")
+  SUBCASE("Debug output should have teal color")
   {
     std::string color = "0;255;255";
     auto value = ngyn::logger.debug("Test");
 
-    REQUIRE(value.find(color) != std::string::npos);
+    CHECK(value.find(color) != std::string::npos);
   }
 
-  SECTION("Warn output should have yellow color")
+  SUBCASE("Warn output should have yellow color")
   {
     std::string color = "255;255;0";
     auto value = ngyn::logger.warning("Test");
 
-    REQUIRE(value.find(color) != std::string::npos);
+    CHECK(value.find(color) != std::string::npos);
   }
 
-  SECTION("Error output should have red color")
+  SUBCASE("Error output should have red color")
   {
     std::string color = "255;0;0";
     auto value = ngyn::logger.error("Test");
 
-    REQUIRE(value.find(color) != std::string::npos);
+    CHECK(value.find(color) != std::string::npos);
   }
 }
