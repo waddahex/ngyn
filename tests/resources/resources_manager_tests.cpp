66855e0b7c756025c21b5b4f00bf54e62def85d6
Iury Ferreira Santana <criticow@outlook.com>
2025-03-12 15:38:52 -0300

    resources manager tests updated

--- tests/resources/resources_manager_tests.cpp
+++ tests/resources/resources_manager_tests.cpp
@@ -1,57 +1,59 @@
-#include <catch2/catch_test_macros.hpp>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
 #include <ngyn/ngyn.hpp>
 
 using namespace ngyn;
 
-TEST_CASE("All operations", "[resources_manager]")
+TEST_CASE("All operations")
 {
-  SECTION("Should return an empty storage")
+  SUBCASE("Should return an empty storage")
   {
     auto storage = ResourcesManager::getStorage<int>();
-    REQUIRE(storage.resources.empty());
+    CHECK(storage.resources.empty());
   }
 
-  SECTION("Should add a resource and return a valid weak_ptr")
+  SUBCASE("Should add a resource and return a valid weak_ptr")
   {
     auto ptr = ResourcesManager::addResource<int>("one", 1);
     auto storage = ResourcesManager::getStorage<int>();
 
-    REQUIRE(!storage.resources.empty());
-    REQUIRE(ptr.lock());
+    CHECK(!storage.resources.empty());
+    CHECK(ptr.lock());
   }
 
-  SECTION("Should return the correct resource")
+  SUBCASE("Should return the correct resource")
   {
     ResourcesManager::addResource<int>("two", 2);
 
     auto resource = ResourcesManager::getResource<int>("two");
     int value = *resource.lock().get();
 
-    REQUIRE(value == 2);
+    CHECK(value == 2);
   }
 
-  SECTION("Should return an invalid weak_ptr")
+  SUBCASE("Should return an invalid weak_ptr")
   {
     auto resource = ResourcesManager::getResource<int>("invalid_resource");
 
-    REQUIRE(!resource.lock());
+    CHECK(!resource.lock());
   }
 
-  SECTION("Should make resource weak_ptr invalid")
+  SUBCASE("Should make resource weak_ptr invalid")
   {
     auto ptr = ResourcesManager::addResource<float>("0.5", 0.5f);
     ResourcesManager::removeResource<float>("0.5");
 
-    REQUIRE(!ptr.lock());
+    CHECK(!ptr.lock());
   }
 
-  SECTION("Should remove resource from storage")
+  SUBCASE("Should remove resource from storage")
   {
     ResourcesManager::addResource<float>("0.4", 0.4f);
     ResourcesManager::removeResource<float>("0.4");
 
     auto storage = ResourcesManager::getStorage<float>();
 
-    REQUIRE(storage.resources.find("0.4") == storage.resources.end());
+    CHECK(storage.resources.find("0.4") == storage.resources.end());
   }
 }
